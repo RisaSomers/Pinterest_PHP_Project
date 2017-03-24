1b4b404d6beaa6a7089a35834f448171f80645cc
RisaSomers <Risa@MacBook-Pro-van-Risa.local>
2017-03-24 09:17:42 +0100

    Databank
    Connectie maken met databank

--- signup.php
+++ signup.php
@@ -1,4 +1,42 @@
-<!DOCTYPE html>
+<?php
+
+    // if geregistreerd
+    if( !empty( $_POST ) ) {
+        // checken of velden ingevuld zijn
+        $FullName = $_POST['FullName'];
+        $UserName = $_POST['UserName'];
+        $Password = $_POST['Password'];
+        $Passwordcon = $_POST['Password_confirmation'];
+
+        $options = [
+            'cost' => 12,
+        ];
+
+        $Password = password_hash($password, PASSWORD_DEFAULT, $options);
+
+        // connectie met databank
+        try {
+            $conn = new PDO('mysql:host=localhost;dbname=Users', 'root', '');
+        } catch (Exception $e) {
+            $error = $e->getMessage();
+            echo $error;
+        }
+
+        $statement = $conn->prepare("INSERT INTO Users (FullName, Username, Email, Password) VALUES(:FullName, :username, :Email, :Password)");
+        $statement->bindValue(":FullName", $this->$FullName);
+        $statement->bindValue(":UserName", $this->$UserName);
+        $statement->bindValue(":Email", $this->$Email);
+        $statement->bindValue(":Password", $this->$Password);
+
+        $res = $statement->execute();
+        return ($res);
+        
+
+    }
+
+
+
+?><!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -68,33 +106,33 @@
 
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
-                <form role="form">
+                <form role="form" method="post">
                     <h2>Please Sign Up <small>It's free and always will be.</small></h2>
                     <hr class="colorgraph">
                     <div class="row">
                         <div class="col-xs-12 col-sm-6 col-md-6">
                             <div class="form-group">
-                                <input type="text" name="fullname" id="fullname" class="form-control input-lg" placeholder="Volledige naam" tabindex="1">
+                                <input type="text" name="FullName" id="fullname" class="form-control input-lg" placeholder="Volledige naam" tabindex="1">
                             </div>
                         </div>
                         <div class="col-xs-12 col-sm-6 col-md-6">
                             <div class="form-group">
-                                <input type="text" name="username" id="username" class="form-control input-lg" placeholder="Gebruikersnaam" tabindex="2">
+                                <input type="text" name="UserName" id="username" class="form-control input-lg" placeholder="Gebruikersnaam" tabindex="2">
                             </div>
                         </div>
                     </div>
                     <div class="form-group">
-                        <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" tabindex="4">
+                        <input type="email" name="Email" id="email" class="form-control input-lg" placeholder="Email Address" tabindex="4">
                     </div>
                     <div class="row">
                         <div class="col-xs-12 col-sm-6 col-md-6">
                             <div class="form-group">
-                                <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="5">
+                                <input type="password" name="Password" id="password" class="form-control input-lg" placeholder="Password" tabindex="5">
                             </div>
                         </div>
                         <div class="col-xs-12 col-sm-6 col-md-6">
                             <div class="form-group">
-                                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-lg" placeholder="Confirm Password" tabindex="6">
+                                <input type="password" name="Password_confirmation" id="password_confirmation" class="form-control input-lg" placeholder="Confirm Password" tabindex="6">
                             </div>
                         </div>
                     </div>
@@ -102,8 +140,8 @@
 
                     <hr class="colorgraph">
                     <div class="row">
-                        <div class="col-xs-12 col-md-6"><input type="submit" value="Register" class="btn btn-primary btn-block btn-lg" tabindex="7"></div>
-                        <div class="col-xs-12 col-md-6"><a href="#" class="btn btn-success btn-block btn-lg">Sign In</a></div>
+                        <div class="col-xs-12 col-md-6"><input name="Registration" type="submit" value="Register" class="btn btn-primary btn-block btn-lg" tabindex="7"></div>
+                        <div class="col-xs-12 col-md-6"><a href="#" name="SignIn" class="btn btn-success btn-block btn-lg">Sign In</a></div>
                     </div>
                 </form>
             </div>
