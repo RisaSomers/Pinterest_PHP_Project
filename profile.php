d219b74a1a435000854c3e171ca3fc86a6c01980
Yoerias <schoeter.yoeri@hotmail.com>
2017-03-27 01:27:03 +0200

    Redirect profile
    nu word je redirected naar login pagina indien je profile.php wilt
    accessen zonder ingelogt te zijn

--- profile.php
+++ profile.php
@@ -4,8 +4,8 @@ session_start();
 
 if(!isset($_SESSION['user_id']) || !isset($_SESSION['logged_in'])){
     //User not logged in. Redirect them back to the login.php page.
-    // header('Location: login.php');
-    // exit;
+    header('Location: login.php');
+    exit;
 }
 
 if(!empty($_FILES)) {
