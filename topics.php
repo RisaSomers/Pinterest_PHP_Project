7d1b35da00473ac058d6b73c8abda5128fab27a9
RisaSomers <Risa@MacBook-Pro-van-Risa.local>
2017-03-26 13:52:20 +0200

    Topics
    html, css en js voor topics aangemaakt + in de database een nieuwe file
    aangemaakt voor topics

--- topics.php
+++ topics.php
@@ -3,13 +3,103 @@
 <head>
     <meta charset="UTF-8">
     <title>Topics</title>
+    <link rel="stylesheet" href="css/topics.css">
+    <link rel="stylesheet" href="js/topics.js">
+    
+    <style>
+    
+        .vierkant{
+            background-color: cadetblue;
+            text-align: center;
+            border-radius: 5%;
+            margin: 20%;
+            margin-top: 15%;
+            padding-top: 5%;
+            
+        }
+    
+    
+    </style>
 </head>
 <body>
+ 
+ 
+ 
+ 
+ <div class="vierkant">
+ 
+ <div class="tekst">
+     
+     <h5>Like 5 topics</h1>
+     <h7>Then we'll build a custom home feed for you</h2>
+     
+ </div>
+ 
+ 
+ 
+  <div class="search">
+    <input type="search" id="filter" placeholder='Search' />
+  </div>
+<article class='content'>
+  <div id='slats'>
+    <ul>
+
+
+  <li class='fontawesome-glass'>
+    <h3>Glass</h3>
+    <p>wine, martini, cup</p>
+  </li>
+
+  <li class='fontawesome-music'>
+    <h3>Music</h3>
+    <p>note, clef, mp3</p>
+  </li>
+
+  <li class='fontawesome-search'>
+    <h3>Search</h3>
+    <p>magnifying glass, zoom</p>
+  </li>
+
+  <li class='fontawesome-envelope-alt'>
+    <h3>Envelope-alt</h3>
+    <p>letter, contact, email, e-mail</p>
+  </li>
+
+  <li class='fontawesome-heart'>
+    <h3>Heart</h3>
+    <p>favourite, love, like, rate, rating</p>
+  </li>
+
+  <li class='fontawesome-star'>
+    <h3>Star</h3>
+    <p>favourite, love, like, rate, rating</p>
+  </li>
+
+  <li class='fontawesome-star-empty'>
+    <h3>Star-empty</h3>
+    <p>favourite, love, like</p>
+  </li>
+
+  
+
+
+      
+      
+    </ul>  
+
+  </div>
+</article>   
+ 
+ <button type="submit"> Follow 5 topics </button>
+
+ 
+  
    
-   <div>
-       <h1>Like 5 topics</h1>
-       <h3>Then we'ill build a custom home feed for you</h3>
-   </div>
     
+     
+      
+    </div>       
+        
+          
 </body>
 </html>
