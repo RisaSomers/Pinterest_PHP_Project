8edf20612e5ae5135f6a5b43afd70a241833f9d1
bryanverli <bryan.verlinden@gmail.com>
2017-03-27 22:23:18 +0200

    profile restricted

--- profile.php
+++ profile.php
@@ -39,6 +39,13 @@ include_once("classes/user.php");
 include_once("classes/profilechange.class.php");
 include_once("classes/profilechange.class.php");
 session_start();
+
+if(!isset($_SESSION['user_id']) || !isset($_SESSION['logged_in'])){
+    //User not logged in. Redirect them back to the login.php page.
+    header('Location: login.php');
+    exit;
+}
+
 $a = new profilechange();
 $allusers = $a->getAll();
 
